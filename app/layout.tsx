0798f6c12c63d35643f1f8dd1be43f11151e57d7
Carlos Jimenez <Goldenaxeastro@gmail.com>
2026-06-12 19:58:20 -0500

    Add iOS PWA standalone meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,9 @@ export const metadata: Metadata = {
   formatDetection: {
     telephone: false,
   },
+  other: {
+    "apple-mobile-web-app-capable": "yes",
+  },
   icons: {
     icon: [
       { url: "/favicon.ico" },
